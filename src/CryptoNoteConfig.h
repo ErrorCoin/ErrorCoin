118e237b99b3fdc9e8f767e8c49f6f83ef041530
ErrorCoin <logan932001@yahoo.com>
2018-08-24 23:16:01 -0700

    updated

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -132,9 +132,9 @@ static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 /* Block heights we are going to have hard forks at */
 const uint64_t FORK_HEIGHTS[] =
 {
-  9999999,
+  53000,
   9999998,
-  9999999 // We dont plan on forking naytime soon
+  9999999
 };
 
 const uint64_t FORK_HEIGHTS_SIZE = sizeof(FORK_HEIGHTS) / sizeof(*FORK_HEIGHTS);
